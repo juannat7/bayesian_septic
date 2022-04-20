5d5caec32553bf949481e8dfb5b2e29a4053165a
Juan Nathaniel <nathanieljuan@gmail.com>
2022-04-20 19:08:24 -0400

    added optimized xgbm baseline, updated read_data function to process DEM and flow

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -9,6 +9,8 @@ import scipy.stats as stats
 import geopandas as gpd
 import seaborn as sns
 import scipy
+import rioxarray
+import xarray as xr
 import censusdata
 from sklearn.metrics import confusion_matrix
 from params import *
@@ -41,7 +43,7 @@ def evaluate_bayes(trace, model, y_actual, samples=500):
     acc = (corr / len(y_pred)) * 100
     return acc, y_pred
 
-def read_data(file_dir, cols, is_balanced=True):
+def read_data(file_dir, cols, is_balanced=True, train_frac=0.9):
     """
     Read the initial CSV file used for modeling
     
@@ -53,6 +55,8 @@ def read_data(file_dir, cols, is_balanced=True):
         the columns of interest where null values will be dropped
     is_balanced: boolean
         balances class-of-interest; to avoid class imbalance
+    train_frac: float
+        the fraction for training split
     
     returns:
     --------
@@ -66,17 +70,6 @@ def read_data(file_dir, cols, is_balanced=True):
         the dictionary mapping between basin_idx and basins
     """
     df = pd.read_csv(file_dir)
-    
-    # keep only relevant columns
-    df = df.dropna(subset=cols).reset_index(drop=True)
-    
-    # normalize
-    for i, col in enumerate(cols):
-        try:
-            new_var = col + '_norm'
-            df[new_var] = (df[col] - df[col].min()) / (df[col].max() - df[col].min())
-        except:
-            continue
 
     # encode categorical sewage system
     enc, _ = pd.factorize(df['sewageSystem'])
@@ -91,16 +84,11 @@ def read_data(file_dir, cols, is_balanced=True):
 
         # split equally
         df = pd.concat((df[df['sewageSystem_enc'] == 0], df[df['sewageSystem_enc'] == 1][:num_repair]))
-
-    # separate septics based on their locations within a basin
-    basin_idx, basins = pd.factorize(df['HU_12_NAME'])
-    coords = {'basin': basins, 'septic': np.arange(len(df))}
     
     # get hydraulic information
     print('processing soil hydraulic data...')
     df['hydraulic_c'] = df['gSSURGO_GA'].str[0] # get the first value if a system has more than one soil type
     df['hydraulic_c'] = df['hydraulic_c'].replace(soil_dict)
-    df['hydraulic_c_norm'] = (df['hydraulic_c'] - df['hydraulic_c'].min()) / (df['hydraulic_c'].max() - df['hydraulic_c'].min())
     
     # get housing information
     print('acquiring housing information...')
@@ -109,17 +97,58 @@ def read_data(file_dir, cols, is_balanced=True):
                                  censusdata.censusgeo([('state', ga_acs_dict['state']), ('county', '*')]),
                                  [ga_acs_dict['code']])
               .reset_index()
-              .rename(columns={ga_acs_dict['code']: ga_acs_dict['col_name']}))
+              .rename(columns={ga_acs_dict['code']: 'median_hse'}))
     
     df = match_acs_features(
             df, 
             ga_acs, 
             pri_key='tblSGA_Property.county_property',
             for_key='County',
-            var_name=ga_acs_dict['col_name']
+            var_name='median_hse'
         )
+    
+    # get DEM and flow
+    print('processing DEM and flow information...')
+    ga_bdry = gpd.read_file('../../Data/ga_bdry')
+    
+    dem = rioxarray.open_rasterio('../../Data/hydrosheds/hyd_na_dem_15s/hyd_na_dem_15s.tif')
+    flow = rioxarray.open_rasterio('../../Data/hydrosheds/na_acc_15s')
+    
+    ga_dem = dem.rio.clip(ga_bdry.geometry.values, ga_bdry.crs)
+    ga_dem = xr.where(ga_dem > 30000, 0, ga_dem)
+    ga_flow = flow.rio.clip(ga_bdry.geometry.values, ga_bdry.crs)
+    ga_flow = xr.where(ga_flow < 0, 0, ga_flow)
+    
+    ## build spatial nearest neighbour lookup
+    kdtree_dem, dem_coords = build_kdtree(ga_dem)
+    kdtree_flow, flow_coords = build_kdtree(ga_flow)
+    
+    def get_spatial_nearest(row, xa, kdtree, coords):
+        pt = [row["tblSGA_Property.gis_long_property"], row["tblSGA_Property.gis_lat_property"]]
+        dist, index = kdtree.query(pt)
+        coor = coords[index]
+        vals = xa.sel(x=coor[0], y=coor[1])
+        return vals.item()
+    
+    df['dem'] = df.apply(lambda x: get_spatial_nearest(x, xa=ga_dem, kdtree=kdtree_dem, coords=dem_coords), axis=1)
+    df['flow'] = df.apply(lambda x: get_spatial_nearest(x, xa=ga_flow, kdtree=kdtree_flow, coords=flow_coords), axis=1)
+    
+    # keep only relevant columns
+    all_cols = cols + ['HU_12_NAME', 'sewageSystem_enc']
+    df = df.dropna(subset=all_cols).reset_index(drop=True)
+    
+    # normalize
+    for i, col in enumerate(cols):
+        try:
+            new_var = col + '_norm'
+            df[new_var] = (df[col] - df[col].min()) / (df[col].max() - df[col].min())
+        except:
+            continue
+ 
 
-    df = normalize(df, var=ga_acs_dict['col_name'], var_norm=ga_acs_dict['col_name'] + '_norm')
+    # separate septics based on their locations within a basin
+    basin_idx, basins = pd.factorize(df['HU_12_NAME'])
+    coords = {'basin': basins, 'septic': np.arange(len(df))}
 
     return df, basin_idx, basins, coords
 
